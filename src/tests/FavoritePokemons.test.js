f776982213b85faba6fed3e0bcd0b90c43eacda6
leandro <leandrochagask@gmail.com>
2021-11-30 18:35:43 -0300

    feat(FavoritePokemons.test.js): adiciona testes do requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  function addFavorite() {
+    const moreDatails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDatails);
+
+    const favoritado = screen.getByText(/pokémon favoritado\?/i);
+    userEvent.click(favoritado);
+
+    const home = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(home);
+  }
+
+  it('a) Exibe mensagem No favorite pokemon found, se não tiver favoritos.', () => {
+    const favorites = screen.getByRole('link', { name: /Favorite/i });
+    userEvent.click(favorites);
+
+    const message = screen.getByText(/no favorite pokemon found/i);
+    expect(message).toBeDefined();
+  });
+
+  it('b) Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    addFavorite();
+    const buttonProximo = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(buttonProximo);
+
+    addFavorite();
+    const favorites = screen.getByRole('link', { name: /Favorite/i });
+    userEvent.click(favorites);
+
+    const favoriteMoreDatails = screen.getAllByText(/more details/i);
+    expect(favoriteMoreDatails.length).toBe(2);
+  });
+});
